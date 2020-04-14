611da6998736f8538a91397d43155bc13b254350
Stephen Burke <burke.stephenpaul@gmail.com>
2020-04-14 14:25:36 -0500

    initial commit

---
+++ src/main/java/edu/nwmissouri/burke/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.burke;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
