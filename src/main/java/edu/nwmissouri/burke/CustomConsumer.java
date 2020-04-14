5e8fea831facfd7158a7ae947087e0fb01e68a08
Stephen Burke <burke.stephenpaul@gmail.com>
2020-04-14 16:00:25 -0500

    added consumer and producer

---
+++ src/main/java/edu/nwmissouri/burke/CustomConsumer.java
@@ -0,0 +1,14 @@
+package edu.nwmissouri.burke;
+
+/**
+ * Hello world!
+ *
+ */
+public class CustomConsumer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
---
+++ src/main/java/edu/nwmissouri/burke/CustomProducer.java
@@ -0,0 +1,14 @@
+package edu.nwmissouri.burke;
+
+/**
+ * Hello world!
+ *
+ */
+public class CustomProducer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
