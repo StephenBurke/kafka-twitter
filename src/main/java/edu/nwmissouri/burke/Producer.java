982021a9a6da553172acfb6903c1deccbed2da77
Stephen Burke <burke.stephenpaul@gmail.com>
2020-04-14 16:46:50 -0500

    added Consumer and Producer tests

---
+++ src/main/java/edu/nwmissouri/burke/Producer.java
@@ -0,0 +1,14 @@
+package edu.nwmissouri.burke;
+
+/**
+ * Hello world!
+ *
+ */
+public class Producer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
