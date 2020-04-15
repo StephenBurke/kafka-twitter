d311210996ebd4a08b1d0ecd6cf59cacee8bf45b
Stephen Burke <burke.stephenpaul@gmail.com>
2020-04-15 16:29:13 -0500

    created Consumer Producer path

--- src/main/java/edu/nwmissouri/burke/Producer.java
+++ src/main/java/edu/nwmissouri/burke/Producer.java
@@ -1,7 +1,17 @@
 package edu.nwmissouri.burke;
 
-import java.io.IOException;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.io.IOException;
 
 public class Producer {
     /**
@@ -9,9 +19,63 @@ public class Producer {
      * run.properties file in src/main/resources.
      */
 
+    private static Scanner in;
 
     public static void main(String[] args) throws IOException
     {
-        System.out.println("hello world");
+        if (args.length != 1) 
+        {
+            System.err.println("Please specify 1 parameters ");
+            System.exit(-1);
+        }
+        String topicName = args[0];
+
+        in = new Scanner(System.in);
+        System.out.println("Enter message(type exit to quit)");
+
+        //Configure the Producer
+        Properties configProperties = new Properties();
+        configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.ByteArraySerializer");
+        configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer");
+
+        org.apache.kafka.clients.producer.Producer producer = new KafkaProducer(configProperties);
+        for (int i = 1; i <= 10; i++) 
+        {
+            String message = createSentence();
+            ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName, message);
+            producer.send(rec);
+        }
+
+
+        String line = in.nextLine();
+        while(!line.equals("exit")) 
+        {
+            //TODO: Make sure to use the ProducerRecord constructor that does not take parition Id
+            ProducerRecord<String, String> rec = new ProducerRecord<String, String>(topicName,line);
+            producer.send(rec);
+            line = in.nextLine();
+        }
+        in.close();
+        producer.close();
+        
     }
+
+    private static String createSentence()
+    {
+        return "Noun verb subject.";
+    }
+
+    public List<String> getTimeLine() throws TwitterException 
+    {
+        Twitter twitter = getTwitterinstance();
+        
+        return twitter.getHomeTimeline().stream().map(item -> item.getText()).collect(Collectors.toList());
+    }
+
+    private Twitter getTwitterinstance() 
+    {
+        return null;
+    }
+   
 }
